131cb6ad98679b1f6e94d23c6489a6b3e3e1dbca
AlbertZaqaryan <itm.albert.zaqaryan@gmail.com>
2022-07-05 11:15:17 +0400

    test

--- core/main/models.py
+++ core/main/models.py
@@ -3,9 +3,7 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField('Category name', max_length=30)
-    img = models.ImageField('Category image', upload_to='media', null=True)
-    about = models.TextField('Category about', null=True)
-    price = models.IntegerField('Category price', null=True)
+    is_public = models.BooleanField(null=True)
 
     def __str__(self):
         return self.name
@@ -13,3 +11,19 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
+
+
+
+class Bag(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='bagcat', null=True)
+    name = models.CharField('Bag name', max_length=30)
+    img = models.ImageField('Bag image', upload_to='media', null=True)
+    about = models.TextField('Bag about', null=True)
+    price = models.IntegerField('Bag price', null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Bag'
+        verbose_name_plural = 'Bags'
